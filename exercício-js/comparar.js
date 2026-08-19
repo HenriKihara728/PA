39d4e0a0a5153bed6e36ccd1ad1e29274babfee0
HenriKihara728 <henrikihara795@gmail.com>
2026-08-19 15:52:39 -0300

    Adiciona nova atividade

---
+++ exercício-js/comparar.js
@@ -0,0 +1,10 @@
+let numero1 = 10;
+let numero2 = 5;
+
+if (numero1 > numero2) {
+    console.log("O primeiro número é maior.");
+} else if (numero1 < numero2) {
+    console.log("O segundo número é maior.");
+} else {
+    console.log("Os números são iguais.");
+}
